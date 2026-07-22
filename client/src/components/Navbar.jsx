de9b6d90ecd7a35d6405ec9e80c2b98c0c89e8b4
himanshukhare <himanshukhare450@gmail.com>
2026-07-22 23:45:43 +0530

    remove logo icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">✦ XChat</Link>
+        <Link to="/" className="nav-logo">XChat</Link>
         <div className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/search">Explore</Link>
