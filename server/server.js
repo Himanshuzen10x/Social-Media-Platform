145ef4bc439c1cd0d56b8638a9c193ddf7484797
himanshukhare <himanshukhare450@gmail.com>
2026-07-22 22:02:00 +0530

    debug: show actual DB error

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(async (req, res, next) => {
     await connectDB();
     next();
   } catch (err) {
-    res.status(500).json({ message: 'Database connection failed' });
+    res.status(500).json({ message: 'Database connection failed', error: err.message, uri_exists: !!process.env.MONGO_URI });
   }
 });
 
